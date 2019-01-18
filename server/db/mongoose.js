a5a53a12bed83d71f6bb083b0d4dfd75c335a9dc
Stefan Deak <deakstefan@gmail.com>
2019-01-18 18:58:40 +0100

    add post todo routes and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,78 @@
+let mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {
+    mongoose
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let newTodo = Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((res) => {
+//     console.log('Saved todo', res);
+// }, (err) => {
+//     console.log('Unable to save todo');
+// });
+
+// let otherTodo = Todo({
+//     text: '  Edit this video   '
+// });
+
+// otherTodo.save().then((res) => {
+//     console.log('Saved todo', res);
+// }, (err) => {
+//     console.log('Unable to save', err);
+// });
+
+// let newUser = User({
+//     email: '   example@gmail.com'
+// });
+
+// newUser.save().then((res) => {
+//     console.log('Saved user', res);
+// }, (err) => {
+//     console.log('Unable to save user', err);
+// });
