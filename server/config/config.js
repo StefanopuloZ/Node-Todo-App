8e40d39dc3968f4026f34f1bffc44801ff4291fb
Stefan Deak <deakstefan@gmail.com>
2019-01-30 13:02:20 +0100

    config update

--- server/config/config.js
+++ server/config/config.js
@@ -7,4 +7,6 @@ if (env === 'development') {
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+    process.env.MONGODB_URI = 'mongodb://admin:123456a@ds113454.mlab.com:13454/todoapp';
 };
